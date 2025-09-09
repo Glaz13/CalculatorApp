dad45fc09bac50fab9833b586ba41f32bdd808e4
Антон Глазков <antoha.glazkow@yandex.ru>
2025-09-09 20:43:09 +1000

    Создана ветка feature-operations

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CalculatorApp
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Калькулятор сложения");
+
+        Console.Write("Введите первое число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = num1 + num2;
+        Console.WriteLine($"Результат: {num1} + {num2} = {result}");
     }
 }
