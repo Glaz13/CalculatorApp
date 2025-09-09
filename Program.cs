ff330281cb1af706c09c4b40de7b86e9f2cda471
Антон Глазков <antoha.glazkow@yandex.ru>
2025-09-09 20:53:03 +1000

    Добавлены операции: вычитание, умножение, деление

--- Program.cs
+++ Program.cs
@@ -4,15 +4,45 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Калькулятор сложения");
+        Console.WriteLine("Калькулятор");
+        Console.WriteLine("Доступные операции: +, -, *, /");
 
         Console.Write("Введите первое число: ");
         double num1 = Convert.ToDouble(Console.ReadLine());
 
+        Console.Write("Введите операцию (+, -, *, /): ");
+        string operation = Console.ReadLine();
+
         Console.Write("Введите второе число: ");
         double num2 = Convert.ToDouble(Console.ReadLine());
 
-        double result = num1 + num2;
-        Console.WriteLine($"Результат: {num1} + {num2} = {result}");
+        double result = 0;
+
+        if (operation == "+")
+        {
+            result = num1 + num2;
+        }
+        else if (operation == "-")
+        {
+            result = num1 - num2;
+        }
+        else if (operation == "*")
+        {
+            result = num1 * num2;
+        }
+        else if (operation == "/")
+        {
+            if (num2 != 0)
+                result = num1 / num2;
+            else
+                Console.WriteLine("Ошибка: деление на ноль!");
+        }
+        else
+        {
+            Console.WriteLine("Неизвестная операция!");
+            return;
+        }
+
+        Console.WriteLine($"Результат: {num1} {operation} {num2} = {result}");
     }
 }
